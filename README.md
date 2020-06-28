847950fd7e1704633171520b76437d66031ba3a7
Junghyun Park <qwertyazsxc@gmail.com>
2020-06-28 22:42:08 +0900

    first commit: 기본 설정 완료

---
+++ README.md
@@ -0,0 +1,15 @@
+# blog-app-front
+
+## 명령어
+
+### webpack-dev-server 실행
+
+```
+$ yarn dev
+```
+
+### 빌드
+
+```
+$ yarn build
+```
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/tsx/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                use: ['babel-loader'],
+                exclude: ['/node_modules'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                exclude: ['/node_modules'],
+            },
+        ],
+    },
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname + '/build'),
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname + '/build'),
+        publicPath: '/',
+        inline: true,
+        hot: true,
+        host: 'localhost',
+        port: 3000,
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+};
