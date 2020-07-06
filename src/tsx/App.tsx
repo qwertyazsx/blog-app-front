4303723118f1dd2639d4c36ad38b69d1c54199e3
Junghyun Park <qwertyazsxc@gmail.com>
2020-07-06 18:00:58 +0900

    little changes

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-export interface AppProps {}
-
-export const App: React.FC<AppProps> = props => {
+export const App: React.FC = () => {
     return <div className="App">hello world!</div>;
 }
