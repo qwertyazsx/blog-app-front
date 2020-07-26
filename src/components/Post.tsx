c0f9f6795d2fa3b4c66094f474f2fa66fbdb826d
Junghyun Park <qwertyazsxc@gmail.com>
2020-07-26 22:02:44 +0900

    Post 스타일 추가

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
+import '../../public/styles/scss/Post.scss';
 
 // 내용
 export const Post = () => {
-    return <div className="post">Post</div>;
+    return (
+        <div className="post">
+            <div className="article_title">This is Article_Title</div>
+            <article>This is Article</article>
+        </div>
+    );
 };
