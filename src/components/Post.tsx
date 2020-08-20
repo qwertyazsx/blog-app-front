f4a8606cb07dcfe5b01288665f7fdbcb67f9df8e
Junghyun Park <qwertyazsxc@gmail.com>
2020-08-20 23:53:15 +0900

    포스트 post요청 보내고 에러 시 /로 redirect

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import '../../public/styles/scss/Post.scss';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 type PostProps = {
@@ -14,15 +15,18 @@ export const Post = (props: PostProps) => {
     React.useEffect(() => {
         fetchPost(props.match.params.postNo);
     });
+    const history = useHistory();
 
     const fetchPost = async (postNo: string) => {
         try {
+            // TODO: 포스트 없을 때 처리
             const url = `/api/v1/posts/${postNo ? postNo : 'recent'}`;
             const response = await axios.get(url);
             document.getElementsByClassName('p_title')[0].innerHTML = response.data.title;
             document.getElementsByClassName('p_article')[0].innerHTML = response.data.content;
         } catch (error) {
             console.log(error);
+            history.push('/');
         }
     };
 
