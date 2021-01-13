486948f0135e84a8d11f1e0f07ab68ea549964a4
Junghyun Park <qwertyazsxc@gmail.com>
2021-01-13 23:52:28 +0900

    사이드바 전체 포스트 버튼 추가

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import '../../public/styles/scss/Sidebar.scss';
+import axios from 'axios';
 
 type SidebarProps = {
     closeSidebar: () => void;
@@ -7,6 +8,16 @@ type SidebarProps = {
 };
 
 export const Sidebar = (props: SidebarProps) => {
+    const [postCount, setPostCount] = React.useState(0);
+
+    const fetchCount = async () => {
+        const url = `/api/v1/posts/count`;
+        const response = await axios.get(url);
+        setPostCount(response.data);
+    };
+
+    fetchCount();
+
     return (
         <div className={`sidebar${props.sidebarCollapsed ? '' : ' active'}`}>
             <button className="s_btn_close" onClick={props.closeSidebar}>
@@ -16,7 +27,9 @@ export const Sidebar = (props: SidebarProps) => {
                 <a href="/edit" className="s_a_edit">
                     📝 포스트 작성
                 </a>
-                {/* TODO: 포스트 목록 버튼 추가 */}
+                <a href="/postlist/1" className="s_a_postlist">
+                    전체 포스트<div className="s_count">{postCount}</div>
+                </a>    
             </div>
         </div>
     );
